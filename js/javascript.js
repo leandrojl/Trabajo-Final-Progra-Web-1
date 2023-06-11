121ceea6fa38446413f6fd05fdfe5afc89fe5db2
leandrojl <leandroracing96@hotmail.com>
2023-06-11 18:02:50 -0300

    Borrado del nav detalle-curso

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,17 @@
 //INDEX.HTML JAVASCRIPT
-
 //LOGICA CONTADOR DE CURSOS OBTENIDOS EN EL NAV
 const contadorCursosObtenidos = document.getElementById('contadorCursosObtenidos');
 const botonComprarCurso = document.getElementById('botonComprarCurso');
+let contadorCursosComprados = sessionStorage.getItem('contadorCursosComprados');
 
-// Verificar si ya existe un valor almacenado en sessionStorage
-let contador = sessionStorage.getItem('contador');
-
-// Si no hay un valor almacenado, establecer el contador en 0
-if (!contador) {
-  contador = 0;
+if (!contadorCursosComprados) {
+    contadorCursosComprados = 0;
 }
-contadorCursosObtenidos.textContent = contador;
+contadorCursosObtenidos.textContent = contadorCursosComprados;
 
-// Agregar un evento de clic al botón "Comprar"
 botonComprarCurso.addEventListener('click', function() {
-    // Incrementar el contador
-    contador++;
-    
-    // Actualizar el valor del contador en el elemento y en sessionStorage
-    contadorCursosObtenidos.textContent = contador;
-    sessionStorage.setItem('contador', contador);
+    contadorCursosComprados++;
+    contadorCursosObtenidos.textContent = contadorCursosComprados;
+    sessionStorage.setItem('contadorCursosComprados', contadorCursosComprados);
   });
 
