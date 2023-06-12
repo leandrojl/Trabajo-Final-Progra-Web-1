be576c3d5d21c78edf5ddea0161c6ffd8bf97940
leandrojl <leandroracing96@hotmail.com>
2023-06-12 09:49:20 -0300

    Clase formulario

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,81 @@
-
-
-//formulario.html
-//validar formulario
-let form = document.querySelector('.formulario-ingreso-curso');
-let mensajeError = document.querySelector('#mensajeError');
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
+//FORMULARIO HTML
+class Formulario {
+    constructor() {
+      this.nombre = '';
+      this.apellido = '';
+      this.dni = '';
+      this.email = '';
+      this.telefono = '';
+      this.mensajeError = '';
+    }
+  
+    obtenerDatos() {
+      this.nombre = document.getElementById('nombre-formulario').value;
+      this.apellido = document.getElementById('apellido-formulario').value;
+      this.dni = document.getElementById('dni-formulario').value;
+      this.email = document.getElementById('email-formulario').value;
+      this.telefono = document.getElementById('telefono-formulario').value;
+      this.mensajeError = document.querySelector('#mensajeError');
+    }
+  
+    validarDatos(){
+        let error = false;
+        let mensaje = '';
+        if(this.nombre==""){
+            error=true;
+            mensaje+="<p>El campo nombre no puede estar vacio</p>";
+        }
+        if(this.apellido==""){
+            error=true;
+            mensaje+="<p>El campo apellido no puede estar vacio</p>";
+        }
+        if(this.dni==""){
+            error=true;
+            mensaje+="<p>El campo dni no puede estar vacio</p>";
+        }
+        if(this.email==""){
+            error=true;
+            mensaje+="<p>El campo email no puede estar vacio</p>";
+        }
+        if(this.telefono==""){
+            error=true;
+            mensaje+="<p>El campo telefono no puede estar vacio</p>";
+        }
+        if(error){
+            //mostrar errores
+            this.mensajeError.innerHTML = mensaje;
+            return false;
     
-    validar();
-})
-
-function validar(){
-    let error = false;
-    let mensaje="";
-    let nombre = document.querySelector("#nombre-formulario").value;
-    let apellido = document.querySelector("#apellido-formulario").value;
-    let dni = document.querySelector("#dni-formulario").value;
-    let email = document.querySelector("#email-formulario").value;
-    let telefono = document.querySelector("#telefono-formulario").value;
-    if(nombre==""){
-        error=true;
-        mensaje+="<p>El campo nombre no puede estar vacio</p>";
+        }else{
+            return true;
+        }
     }
-    if(apellido==""){
-        error=true;
-        mensaje+="<p>El campo apellido no puede estar vacio</p>";
-    }
-    if(dni==""){
-        error=true;
-        mensaje+="<p>El campo dni no puede estar vacio</p>";
-    }
-    if(email==""){
-        error=true;
-        mensaje+="<p>El campo email no puede estar vacio</p>";
-    }
-    if(telefono==""){
-        error=true;
-        mensaje+="<p>El campo telefono no puede estar vacio</p>";
-    }
-    if(error){
-        //mostrar errores
-        mensajeError.innerHTML = mensaje;
-
-    }else{
-        //enviar formulario
-        form.submit();
+  
+    enviarDatos() {
+      if (this.validarDatos()) {
+        console.log('Datos enviados:');
+        console.log('Nombre:', this.nombre);
+        console.log('Apellido:', this.apellido);
+        console.log('DNI:', this.dni);
+        console.log('Email:', this.email);
+        console.log('Teléfono:', this.telefono);
         console.log("formulario enviado");
+        document.querySelector('.formulario-ingreso-curso').reset();
+        this.mensajeError.innerHTML = '';
+      }
+      
     }
-}
+  }
+  
+const miFormulario = new Formulario();
+  
+ document.querySelector('.formulario-ingreso-curso').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+    miFormulario.obtenerDatos(); // Obtener los datos del formulario
+    miFormulario.enviarDatos(); // Enviar los datos del formulario
+  });
+//FORMULARIO HTML
+
+
+
 
